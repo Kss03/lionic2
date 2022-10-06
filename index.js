f5e555eff6cee3a944efab37a88cf904f10c2ee5
Kirill Suranov <78699588+Kss03@users.noreply.github.com>
2022-10-06 21:04:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-console.log('eee');
-
 let burger = document.querySelector('.burger');
 let nav = document.querySelector('.header__nav');
 let navLinks = document.querySelectorAll('.header__nav .nav__link');
+let body = document.querySelector('body');
 
-console.log(navLinks);
+console.log(body);
 
 burger.addEventListener('click', function() {
   burger.classList.toggle('burger--active');
   nav.classList.toggle('header__nav--active');
+  body.classList.toggle('display--block');
 })
 
 navLinks.forEach(function(elem) {
   elem.addEventListener('click', function() {
     burger.classList.remove('burger--active');
     nav.classList.remove('header__nav--active');
+    body.classList.remove('display--block');
   })
 })
